f3bd53fdb360f1280e61cb98e55cca481fd1eca9
ChahakT <chahaktharani@gmail.com>
2020-11-08 20:16:35 +0530

    Update Play.py

--- Play.py
+++ Play.py
@@ -1,9 +1,11 @@
 from Game import TicTacToe, Humanplayer, Randomplayer
 from QLearning import  Qlearning
 
-game = TicTacToe() #game instance
-player1=Humanplayer() #human player
-player2=Qlearning()  #agent
-game.startGame(player1,player2)#player1 starts with X, player2 follows with 0
+game = TicTacToe() #init game
+player1=Humanplayer() #human
+player2=Qlearning() #agent
+
+game.startGame(player1,player2)#player1: X, player2: 0
+
 game.reset() #reset
 game.render() # render display

a3e9c3a39885fedc7cd23b8ee0a9fea9130490cd
ChahakT <chahaktharani@gmail.com>
2020-11-08 20:15:13 +0530

    Update Train.py

--- Train.py
+++ Train.py
@@ -1,9 +1,11 @@
 from Game import TicTacToe
 from QLearning import  Qlearning
 
-game = TicTacToe(True) #game instance, True means training
-player1= Qlearning() #player1 learning agent
-player2 =Qlearning() #player2 learning agent
-game.startTraining(player1,player2) #start training
-game.train(200000) #train for 200,000 iterations
-game.saveStates()  #save Qtable
+game = TicTacToe(True) #init game, true implies training
+agent1= Qlearning() #learning agent 1
+agent2= Qlearning() #learning agent 2
+
+game.startTraining(agent1,agent2) #train agents
+game.train(250000) #250,000 iterations
+
+game.saveStates() #save Qtable
